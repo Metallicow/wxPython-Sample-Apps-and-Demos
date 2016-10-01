689b528d1e14c480f9ee9a5f42deb809a57c9e24
Metallicow <metaliobovinus@gmail.com>
2016-10-01 13:47:20 -0500

    Add MultiChoiceDialog Demos

---
+++ 101_Common_Dialogs/MultiChoiceDialog/MultiChoiceDialog_extended.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-Imports----------------------------------------------------------------------
+
+#--wxPython Imports.
+import wx
+
+
+#- wxPython Demo --------------------------------------------------------------
+__wxPyOnlineDocs__ = 'http://wxpython.org/Phoenix/docs/html/MultiChoiceDialog.html'
+__wxPyDemoPanel__ = 'TestPanel'
+
+overview = """\
+<html>
+<body>
+<h1>wx.MultiChoiceDialog</h1>
+
+This class represents a dialog that shows a list of strings, and
+allows the user to select one or more.
+
+</body>
+</html>
+"""
+
+
+class TestPanel(wx.Panel):
+    def __init__(self, parent, log):
+        self.log = log
+        wx.Panel.__init__(self, parent, -1)
+
+        b = wx.Button(self, -1, "Create and Show a wx.MultiChoiceDialog", (50,50))
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+
+
+    def OnButton(self, evt):
+        lst = ['apple', 'pear', 'banana', 'coconut', 'orange', 'grape', 'pineapple',
+               'blueberry', 'raspberry', 'blackberry', 'snozzleberry',
+               'etc', 'etc..', 'etc...']
+
+        dlg = wx.MultiChoiceDialog(self,
+                                   "Pick some fruit from\nthis list",
+                                   "wx.MultiChoiceDialog", lst)
+
+        if (dlg.ShowModal() == wx.ID_OK):
+            selections = dlg.GetSelections()
+            strings = [lst[x] for x in selections]
+            self.log.WriteText("Selections: %s -> %s\n" % (selections, strings))
+
+        dlg.Destroy()
+
+
+#- wxPy Demo -----------------------------------------------------------------
+
+
+def runTest(frame, nb, log):
+    win = TestPanel(nb, log)
+    return win
+
+
+#- __main__ Demo --------------------------------------------------------------
+
+
+class printLog:
+    def __init__(self):
+        pass
+
+    def write(self, txt):
+        print('%s' % txt)
+
+    def WriteText(self, txt):
+        print('%s' % txt)
+
+
+class TestFrame(wx.Frame):
+    def __init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString,
+                 pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=wx.DEFAULT_FRAME_STYLE, name='frame'):
+        wx.Frame.__init__(self, parent, id, title, pos, size, style, name)
+
+        log = printLog()
+
+        panel = TestPanel(self, log)
+        self.Bind(wx.EVT_CLOSE, self.OnDestroy)
+
+
+    def OnDestroy(self, event):
+        self.Destroy()
+
+
+class TestApp(wx.App):
+    def OnInit(self):
+        gMainWin = TestFrame(None)
+        gMainWin.SetTitle('Test Demo')
+        gMainWin.Show()
+
+        return True
+
+
+#- __main__ -------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import sys
+    print('Python %s.%s.%s %s' % sys.version_info[0:4])
+    print('wxPython %s' % wx.version())
+    gApp = TestApp(redirect=False,
+            filename=None,
+            useBestVisual=False,
+            clearSigInt=True)
+
+    gApp.MainLoop()
---
+++ 101_Common_Dialogs/MultiChoiceDialog/MultiChoiceDialog_minimal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#-Imports---------------------------------------------------------------------
+
+#--wxPython Imports.
+import wx
+
+
+if __name__ == '__main__':
+    app = wx.App()
+
+    lst = ['apple', 'pear', 'banana', 'coconut', 'orange', 'grape', 'peach',
+           'pineapple', 'blueberry', 'raspberry', 'blackberry', 'strawberry',
+           'apricot', 'mango', 'gooseberry']
+    dlg = wx.MultiChoiceDialog(None, 
+                               "Pick some fruit from this list...",
+                               "wx.MultiChoiceDialog", sorted(lst))
+    if dlg.ShowModal() == wx.ID_OK:
+        print(dlg.GetSelections())
+    dlg.Destroy()
+
+    app.MainLoop()
