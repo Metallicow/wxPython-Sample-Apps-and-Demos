bec1f7e0552333740e1143b700172a8d89b9ac63
Metallicow <metaliobovinus@gmail.com>
2016-10-15 12:34:04 -0500

    Add GenericButtons Demos

---
+++ 105_Custom_Controls/GenericButtons/GenericButtons_extended.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-Imports----------------------------------------------------------------------
+
+#--Python Imports.
+import os
+import sys
+
+#--wxPython Imports.
+import wx
+import wx.lib.buttons  as  buttons
+
+#--Local Imports.
+## import images
+
+#-Globals----------------------------------------------------------------------
+try:
+    gFileDir = os.path.dirname(os.path.abspath(__file__))
+except Exception:
+    gFileDir = os.path.dirname(os.path.abspath(sys.argv[0]))
+gBmpDir = gFileDir + os.sep + 'bitmaps'
+
+#- wxPython Demo --------------------------------------------------------------
+__wxPyOnlineDocs__ = 'http://wxpython.org/Phoenix/docs/html/wx.lib.buttons.html'
+__wxPyDemoPanel__ = 'TestPanel'
+
+overview = buttons.__doc__
+
+
+class TestPanel(wx.Panel):
+    def __init__(self, parent, log):
+        wx.Panel.__init__(self, parent, -1)
+        self.log = log
+        ##self.SetBackgroundColour("sky blue")
+
+        sizer = wx.FlexGridSizer(cols=3, hgap=20, vgap=20)
+
+        # A regular button, selected as the default button.
+        b = wx.Button(self, -1, "A real button")
+        b.SetDefault()
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+
+        # Same thing, but NOT set as the default button.
+        b = wx.Button(self, -1, "non-default")
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+        sizer.Add((10, 10))
+
+        # Plain old text button based off GenButton().
+        b = buttons.GenButton(self, -1, 'Hello')
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+
+        # Plain old text button, disabled.
+        b = buttons.GenButton(self, -1, 'disabled')
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        b.Enable(False)
+        sizer.Add(b)
+
+        # This time, we let the botton be as big as it can be.
+        # Also, this one is fancier, with custom colors and bezel size.
+        b = buttons.GenButton(self, -1, 'bigger')
+        self.Bind(wx.EVT_BUTTON, self.OnBiggerButton, b)
+        b.SetFont(wx.Font(20, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        b.SetBezelWidth(5)
+        b.SetMinSize(wx.DefaultSize)
+        b.SetBackgroundColour("Navy")
+        b.SetForegroundColour(wx.WHITE)
+        b.SetToolTip("This is a BIG button...")
+        # let the sizer set best size
+        sizer.Add(b, flag=wx.ADJUST_MINSIZE)
+
+        # An image button.
+        ## bmp = images.Test2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "test2.bmp", wx.BITMAP_TYPE_BMP)
+        b = buttons.GenBitmapButton(self, -1, bmp)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+
+        # An image button, disabled.
+        ## bmp = images.Test2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "test2.bmp", wx.BITMAP_TYPE_BMP)
+        b = buttons.GenBitmapButton(self, -1, bmp)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+        b.Enable(False)
+
+        # An image button, using a mask to get rid of the
+        # undesireable part of the image.
+        b = buttons.GenBitmapButton(self, -1, None)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        ## bmp = images.Bulb1.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb1.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapLabel(bmp)
+        #@ bmp = images.Bulb2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb2.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapSelected(bmp)
+        b.SetInitialSize()
+        sizer.Add(b)
+
+        # A toggle button.
+        b = buttons.GenToggleButton(self, -1, "Toggle Button")
+        self.Bind(wx.EVT_BUTTON, self.OnToggleButton, b)
+        sizer.Add(b)
+
+        # An image toggle button.
+        b = buttons.GenBitmapToggleButton(self, -1, None)
+        self.Bind(wx.EVT_BUTTON, self.OnToggleButton, b)
+        ## bmp = images.Bulb1.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb1.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapLabel(bmp)
+        ## bmp = images.Bulb2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb2.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapSelected(bmp)
+        b.SetToggle(True)
+        b.SetInitialSize()
+        sizer.Add(b)
+
+        # A bitmap button with text.
+        b = buttons.GenBitmapTextButton(self, -1, None, "Bitmapped Text", size=(200, 45))
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        ## bmp = images.Bulb1.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb1.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapLabel(bmp)
+        ## bmp = images.Bulb2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "bulb2.bmp", wx.BITMAP_TYPE_BMP)
+        mask = wx.Mask(bmp, wx.BLUE)
+        bmp.SetMask(mask)
+        b.SetBitmapSelected(bmp)
+        b.SetUseFocusIndicator(False)
+        b.SetInitialSize()
+        sizer.Add(b)
+
+        # A flat text button.
+        b = buttons.GenButton(self, -1, 'Flat buttons too!', style=wx.BORDER_NONE)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b, flag=wx.ALIGN_CENTER_VERTICAL)
+
+        # A flat image button.
+        ## bmp = images.Test2.GetBitmap()
+        bmp = wx.Bitmap(gBmpDir + os.sep + "test2.bmp", wx.BITMAP_TYPE_BMP)
+        bmp.SetMaskColour("blue")
+        b = buttons.GenBitmapButton(self, -1, bmp, style=wx.BORDER_NONE)
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        sizer.Add(b)
+        ##b.SetBackgroundColour("sky blue")
+        ##b.SetBackgroundColour("pink")
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(vbox)
+
+        b = buttons.ThemedGenButton(self, -1, 'Drawn with native renderer')
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        vbox.Add(b, 0, wx.ALL, 5)
+
+        b = buttons.ThemedGenToggleButton(self, -1, 'native renderered toggle')
+        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        vbox.Add(b, 0, wx.ALL, 5)
+
+        border = wx.BoxSizer(wx.VERTICAL)
+        border.Add(sizer, 0, wx.ALL, 25)
+        self.SetSizer(border)
+
+
+    def OnButton(self, event):
+        self.log.WriteText("Button Clicked: %d\n" % event.GetId())
+
+    def OnBiggerButton(self, event):
+        self.log.WriteText("Bigger Button Clicked: %d\n" % event.GetId())
+        b = event.GetEventObject()
+        txt = "big " + b.GetLabel()
+        b.SetLabel(txt)
+        self.GetSizer().Layout()
+
+    def OnToggleButton(self, event):
+        msg = (event.GetIsDown() and "on") or "off"
+        self.log.WriteText("Button %d Toggled: %s\n" % (event.GetId(), msg))
+
+
+#- wxPy Demo -----------------------------------------------------------------
+
+
+def runTest(frame, nb, log):
+    win = TestPanel(nb, log)
+    return win
+
+
+#- __main__ Demo --------------------------------------------------------------
+
+
+class printLog:
+    def __init__(self):
+        pass
+
+    def write(self, txt):
+        print('%s' % txt)
+
+    def WriteText(self, txt):
+        print('%s' % txt)
+
+
+class TestFrame(wx.Frame):
+    def __init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString,
+                 pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=wx.DEFAULT_FRAME_STYLE, name='frame'):
+        wx.Frame.__init__(self, parent, id, title, pos, size, style, name)
+
+        log = printLog()
+
+        panel = TestPanel(self, log)
+        self.Bind(wx.EVT_CLOSE, self.OnDestroy)
+
+
+    def OnDestroy(self, event):
+        self.Destroy()
+
+
+class TestApp(wx.App):
+    def OnInit(self):
+        gMainWin = TestFrame(None)
+        gMainWin.SetTitle('Test Demo')
+        gMainWin.Show()
+
+        return True
+
+
+#- __main__ -------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import sys
+    print('Python %s.%s.%s %s' % sys.version_info[0:4])
+    print('wxPython %s' % wx.version())
+    gApp = TestApp(redirect=False,
+                   filename=None,
+                   useBestVisual=False,
+                   clearSigInt=True)
+    gApp.MainLoop()
+
+
---
+++ 105_Custom_Controls/GenericButtons/__demo__.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-MetaData --------------------------------------------------------------------
+__doc__ = """
+This module contains the meta data needed for integrating the samples
+in the directory into the wxPython demo framework. Once imported,
+this module returns the following information:
+
+* GetDemoBitmap: returns the bitmap used in the wxPython tree control
+  to characterize the package;
+* GetDemos: returns all the demos in the package;
+* GetOverview: returns a wx.html-ready representation of the package's docs.
+
+These meta data are merged into the wxPython demo tree at startup.
+
+Last updated: User's Name @ 08 Aug 20xx, 21.00 GMT.
+Version 0.0.1
+
+"""
+
+__version__ = "0.0.1"
+__author__ = "wxPython Team"
+
+
+#-Imports----------------------------------------------------------------------
+#--wxPython Imports.
+import wx
+from wx.lib.embeddedimage import PyEmbeddedImage
+
+
+def GetDemoBitmap():
+    """
+    Returns the bitmap to be used for the demo tree item's bitmap.
+    """
+
+    # Get the image as PyEmbeddedImage
+    image = customcontrol16 = PyEmbeddedImage(
+    "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAAnRJ"
+    "REFUeJylk09vVGUUxn/nfe/MtNOZtoy0pi3QNJHAQkwKRNCNuCJuSGBj3LkwIWHDJzDu3GCi"
+    "iZ/AlW78AIbACmVhNEaNYpCQDDBAhcIwk7lz733POS5u+RPjxvhsTnKS8+T8Tp4D/1Oytnnh"
+    "wvFjr3/gliQpJBVUQRWS7dQEas97poG8NO//+tnH2cGNXe+ce3djcTwpyQuYFrJToahgOoW8"
+    "gKKEfCrkFVRVYHtk3Lu5+lbWXWgWayst1JqYgRkgQgTK5LgJ0+SoBcwcNcE9UGrGrd9jyhqN"
+    "wOpKh+58+xnX/ccV3/Yrjq63mFTO4eUMcHDD3QEoq8jCnJAB7PQAuHJjyg8D5aEKl65t0ZyZ"
+    "4Y31Nq8tCfuXA2VluNd4SbU2eFGXfxuy1erQ3H7Igz/63Bomrq7t48iBl/n0RMQF1LxGdSW8"
+    "OLw9VvpFiz0vzXLm2DJffvgmn587zGrLuTJQPvk+oUnBHVPD7R8GQeDyzwOuXn9E3mgRxDny"
+    "SpvzJ/eye1eHm0NDk2FWG5hbbSAiACy0I1+8t4deI/DnGC7eDvQfOXNzgQNLgjUytsZeb+AO"
+    "pmQBIcZYp0qgN9/g2mDIjQcTjm50ebJ/nlHhzIeC767fJR7ajartHN7qI4ZYk7g7670m7x+a"
+    "AVXuFDltm+Gbn7ZYXOzy0ds9em2YVoaZAEoWsyit5tMNhM5s4OyJJTDlqx/HhGnJqYNdhnli"
+    "c+8sZkYWnRACEkSy4aiS7ZEQI2iqk2iqmDvHV9o1pwTcI8NRwswQgUleMB5NosTOq6dX922e"
+    "CWJi9jRRhgEBMK3AFUxxEnhCUMqq5K/BL1//t9/9F/0NKrFMKr3z3WMAAAAASUVORK5CYII=")
+
+    # Return the bitmap to use in the wxPython demo tree control.
+    return image
+
+
+def GetDemos():
+    """
+    Returns all the demo names in the package, together with the
+    tree item name which will go in the wxPython demo tree control.
+    """
+
+    # The tree item text.
+    TreeItemText = "GenericButtons"
+
+    # The tree item's demos.
+    TreeItemDemos = (
+        'GenericButtons_extended.py',
+        'GenericButtons_minimal.py',
+        )
+
+    return TreeItemText, TreeItemDemos
+
+
+def GetOverview():
+    """
+    Creates the wxHTML code to display on the tree item's Overview tab.
+    """
+
+    wxHtmlOverviewStr = '''\
+    <html><body>
+    <center><h2>GenericButtons</h2></center>
+    <p>GenericButtons demos.
+    </body></html>
+    '''
+
+    return wxHtmlOverviewStr
