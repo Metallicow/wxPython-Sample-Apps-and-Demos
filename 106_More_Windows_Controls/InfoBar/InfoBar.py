53d7a8c095ee2b34fa29f4c274beb6883ffa1435
Metallicow <metaliobovinus@gmail.com>
2018-11-06 12:31:35 -0600

    Add InfoBar Demos

---
+++ 106_More_Windows_Controls/InfoBar/InfoBar.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-Imports----------------------------------------------------------------------
+
+#--wxPython Imports.
+import wx
+
+#- wxPython Demo --------------------------------------------------------------
+
+__wxPyOnlineDocs__ = 'https://wxpython.org/Phoenix/docs/html/wx.InfoBar.html'
+__wxPyDemoPanel__ = 'TestPanel'
+
+overview = """<html><body>
+<h2><center>wx.InfoBar</center></h2>
+
+An info bar is a transient window shown at top or bottom of its parent
+window to display non-critical information to the user.
+
+<p>This class provides another way to show messages to the user,
+intermediate between message boxes and status bar messages. The
+message boxes are modal and thus interrupt the users work flow and
+should be used sparingly for this reason. However status bar messages
+are often too easy not to notice at all. An info bar provides a way to
+present the messages which has a much higher chance to be noticed by
+the user but without being annoying.
+
+<p>Info bar may show an icon (on the left), text message and, optionally,
+buttons allowing the user to react to the information presented. It
+always has a close button at the right allowing the user to dismiss it
+so it isn't necessary to provide a button just to close it.
+
+</body></html>
+"""
+
+#----------------------------------------------------------------------
+
+flags = [ (wx.ICON_NONE,        "ICON_NONE"),
+          (wx.ICON_INFORMATION, "ICON_INFORMATION"),
+          (wx.ICON_QUESTION,    "ICON_QUESTION"),
+          (wx.ICON_WARNING,     "ICON_WARNING"),
+          (wx.ICON_ERROR,       "ICON_ERROR")
+          ]
+          
+
+class TestPanel(wx.Panel):
+    def __init__(self, parent, log):
+        self.log = log
+        wx.Panel.__init__(self, parent, -1)
+
+        # Create the InfoBar.  It starts out in a hidden state so it
+        # won't be visible until we need it.
+        self.info = wx.InfoBar(self)
+        panel = wx.Panel(self)
+
+        self.message = wx.TextCtrl(panel, -1, "Hello World", size=(250,-1))
+        self.flags = wx.Choice(panel, choices=[f[1] for f in flags])
+        self.flags.SetSelection(1) # wx.ICON_INFORMATION is the default
+        
+        smBtn = wx.Button(panel, -1, "Show Message")
+        dmBtn = wx.Button(panel, -1, "Dismiss")
+        addBtn = wx.Button(panel, -1, "Add Button")
+
+        self.addBtnCount = 0  # Lets keep track of the buttons added.
+
+        fgs = wx.FlexGridSizer(cols=3, vgap=10, hgap=10)
+        fgs.Add(wx.StaticText(panel, -1, "Message:"), 0,
+                wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        fgs.Add(self.message)
+        fgs.Add(self.flags)
+        fgs.AddSpacer(5)
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox.Add(smBtn, 0, wx.RIGHT, 5)
+        hbox.Add(dmBtn)
+        fgs.Add(hbox)
+        fgs.AddSpacer(5)
+        fgs.AddSpacer(5)
+        fgs.Add(addBtn)
+
+        panel.Sizer = wx.BoxSizer(wx.VERTICAL)
+        text = """\
+An info bar is a transient window shown at top or bottom of its parent window
+to display non-critical information to the user."""        
+        panel.Sizer.Add(wx.StaticText(panel, -1, text), 0, wx.TOP|wx.LEFT, 25)
+        panel.Sizer.Add(fgs, 1, wx.EXPAND|wx.ALL, 25)
+                                   
+        self.Sizer = wx.BoxSizer(wx.VERTICAL)
+        self.Sizer.Add(self.info, 0, wx.EXPAND)
+        self.Sizer.Add(panel, 1, wx.EXPAND)
+
+        self.Bind(wx.EVT_BUTTON, self.OnShowMessage, smBtn)
+        self.Bind(wx.EVT_BUTTON, self.OnDismiss, dmBtn)
+        self.Bind(wx.EVT_BUTTON, self.OnAddButton, addBtn)
+
+
+    def OnShowMessage(self, event):
+        msg = self.message.GetValue()
+        flag = flags[self.flags.GetSelection()][0]
+        self.info.ShowMessage(msg, flag)
+
+    def OnDismiss(self, event):
+        self.info.Dismiss()
+
+    def OnAddButton(self, event):
+        self.addBtnCount += 1
+        btnId = wx.NewId()
+        self.info.AddButton(btnId, "new button%s" % self.addBtnCount)
+        self.info.Bind(wx.EVT_BUTTON, self.OnButtonClicked, id=btnId)
+
+    def OnButtonClicked(self, event):
+        evtObj = event.GetEventObject()
+        wx.MessageBox("%s clicked" % evtObj.GetLabel())
+        # Calling event.Skip() will allow the default handler to run
+        # which will dismiss the info bar.  If you don't want it to be
+        # dismissed for a particular button then then don't call
+        # Skip().
+        event.Skip()
+
+      
+#- wxPy Demo -----------------------------------------------------------------
+
+
+def runTest(frame, nb, log):
+    win = TestPanel(nb, log)
+    return win
+
+
+#- __main__ Demo --------------------------------------------------------------
+
+
+class printLog:
+    def __init__(self):
+        pass
+
+    def write(self, txt):
+        print('%s' % txt)
+
+    def WriteText(self, txt):
+        print('%s' % txt)
+
+
+class TestFrame(wx.Frame):
+    def __init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString,
+                 pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=wx.DEFAULT_FRAME_STYLE, name='frame'):
+        wx.Frame.__init__(self, parent, id, title, pos, size, style, name)
+
+        log = printLog()
+
+        panel = TestPanel(self, log)
+        self.Bind(wx.EVT_CLOSE, self.OnDestroy)
+
+
+    def OnDestroy(self, event):
+        self.Destroy()
+
+
+class TestApp(wx.App):
+    def OnInit(self):
+        gMainWin = TestFrame(None)
+        gMainWin.SetTitle('Test Demo')
+        gMainWin.Show()
+
+        return True
+
+
+#- __main__ -------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    import sys
+    print('Python %s.%s.%s %s' % sys.version_info[0:4])
+    print('wxPython %s' % wx.version())
+    gApp = TestApp(redirect=False,
+                   filename=None,
+                   useBestVisual=False,
+                   clearSigInt=True)
+    gApp.MainLoop()
---
+++ 106_More_Windows_Controls/InfoBar/__demo__.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-MetaData --------------------------------------------------------------------
+__doc__ = """
+This module contains the meta data needed for integrating the samples
+in the directory into the wxPython demo framework. Once imported,
+this module returns the following information:
+
+* GetDemoBitmap: returns the bitmap used in the wxPython tree control
+  to characterize the package;
+* GetDemos: returns all the demos in the package;
+* GetOverview: returns a wx.html-ready representation of the package's docs.
+
+These meta data are merged into the wxPython demo tree at startup.
+
+Last updated: User's Name @ 08 Aug 20xx, 21.00 GMT.
+Version 0.0.1
+
+"""
+
+__version__ = "0.0.1"
+__author__ = "wxPython Team"
+
+
+#-Imports----------------------------------------------------------------------
+#--wxPython Imports.
+import wx
+from wx.lib.embeddedimage import PyEmbeddedImage
+
+
+def GetDemoBitmap():
+    """
+    Returns the bitmap to be used for the demo tree item's bitmap.
+    """
+
+    # Get the image as PyEmbeddedImage
+    image = morecontrols16 = PyEmbeddedImage(
+    "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACq0lEQVR4nIXSQUtbWRjG8f89"
+    "9yRGTYj2SgKVzKIg46BZSCkUo0N3tQuZTYabwQ/QzQzDLAfapRSy6MYP0I3QSBcuhiEGKnYr"
+    "LZmKCDpiXFhjhoQ0aGJ7bu49ZxZiY7rps3s58OPlPY8sFAqGb+Tft28RxSLGstALC3x/796X"
+    "NwkwNjZGvV5ndHQUKSUbGxsUi0UuLy9ptVqEPI+/njyBIGDx2TPqz5+TSqXI5/NXQDqdpt1u"
+    "4/s+WmuWlpbIZrP4vo9SivKbN9hKYXyfP37/jan7s4yMjFCtVq+ARCJBIpHoW7vdbgMghKBV"
+    "Oca8/wfjd0nfjfDjgwfYtk2lUrkCbub8/Jxms8nfs7Pcnp4mUIrAGEilEN0uwasmWy9/QmvN"
+    "ebncDzQaDarVKgMDAySSSaYnJhBKwefPXDabmCDgh/govjH4xtCJx3uAUoqdnR1s2+bs7IyN"
+    "gwNeHx/j+z5Bp8OvMzPgebx4945QLIZtWfz38WMP2N/fZ3t7GyEEJycntB89Yv/DB46OjjCd"
+    "Dn8eHIAxTD59ys+PHxONRllfX+8Bp6enFAoFUqkUtVqNWq1Go9HA8zzGgFszM1ha4336hJQS"
+    "IUSvBwCO43B4eMju7i5CCLTWAITDYYakZKdSQdg2lhBYltUPBEFAOp0mm82yurqK1ppQKEQ8"
+    "HsdxHCbu3OFlNMrCw4f8sriIUorBwcEeYFkWUkry+Ty1Wo3NzU0cxyGZTBKJRKg3m9zPZNDR"
+    "KK+3tvq+XV6XRWuNlJLl5WXm5+fZ29sDIBaLkclkmJubo1wu47ouAGtra+RyuWhfD4aHh5mc"
+    "nGRqagqlFJ7nMTQ0RDgc5uLiAoBWq0WpVCKXy0WMMeoLEIlE+Bq7meujlUolXNe1rjeRACsr"
+    "K2fdbrc7Pj7+Hd+I67rWzfl/sCoi6Ch8JCoAAAAASUVORK5CYII=")
+
+    # Return the bitmap to use in the wxPython demo tree control.
+    return image
+
+
+def GetDemos():
+    """
+    Returns all the demo names in the package, together with the
+    tree item name which will go in the wxPython demo tree control.
+    """
+
+    # The tree item text.
+    TreeItemText = "InfoBar"
+
+    # The tree item's demos.
+    TreeItemDemos = (
+        'InfoBar_extended.py',
+        'InfoBar_minimal.py',
+        )
+
+    return TreeItemText, TreeItemDemos
+
+
+def GetOverview():
+    """
+    Creates the wxHTML code to display on the tree item's Overview tab.
+    """
+
+    wxHtmlOverviewStr = '''\
+    <html><body>
+    <center><h2>InfoBar</h2></center>
+    <p>InfoBar demos.
+    </body></html>
+    '''
+
+    return wxHtmlOverviewStr
